db58d70debfa0d22b87ca6e5fc8e924a5281a142
João Carlos de Lima Escaliante <117391568+JoaoEscaliante@users.noreply.github.com>
2025-03-03 14:34:47 -0300

    Add files via upload

--- data/query.sql
+++ data/query.sql
@@ -1,9 +1,4 @@
-SELECT * 
+SELECT *
+FROM customer_profile
 
-FROM customers AS t1
-
-LEFT JOIN transactions AS t2
-ON t1.idCustomer = t2.idCustomer
-
-LEFT JOIN transactions_product AS t3
-ON t2.idTransaction = t3.idTransaction
+group by 1,2
